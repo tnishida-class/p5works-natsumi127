e41d6cfc138ef35f2e46a833eb3e2089a7e235e1
natsumi127 <72774818+natsumi127@users.noreply.github.com>
2020-10-27 12:33:22 +0900

    小手調べ 課題

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200);
+  background(255);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+    noFill();
+    if(i<5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
+    ellipse(100, 100, 15+i*15);
+    }
 }
